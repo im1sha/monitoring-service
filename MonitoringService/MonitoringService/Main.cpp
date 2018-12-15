163317faaba2ca83b2ea02b42239006b1e8d23f6
im1sha <vkporsche@ya.ru>
2018-12-16 01:26:09 +0300

    global memory usage: partial implementation

--- MonitoringService/MonitoringService/Main.cpp
+++ MonitoringService/MonitoringService/Main.cpp
@@ -28,3 +28,46 @@ int wmain(int argc, wchar_t * argv[], wchar_t * envp[])
 	::system("pause");
 	return 0;
 }
+
+
+//
+//#include "pch.h"
+//#include <iostream>
+//#include <windows.h>
+//#include <stdio.h>
+//#include <tchar.h>
+//
+//// Use to convert bytes to MB
+//#define DIV 1048576
+//
+//// Use to convert bytes to MB
+////#define DIV 1024
+//
+//// Specify the width of the field in which to print the numbers. 
+//// The asterisk in the format specifier "%*I64d" takes an integer 
+//// argument and uses it to pad and right justify the number.
+//
+//#define WIDTH 7
+//
+//void _tmain()
+//{
+//	MEMORYSTATUSEX statex;
+//
+//	statex.dwLength = sizeof(statex);
+//
+//	GlobalMemoryStatusEx(&statex);
+//
+//
+//	_tprintf(TEXT("There is  %*ld percent of memory in use.\n"), WIDTH, statex.dwMemoryLoad);
+//	_tprintf(TEXT("There are %*I64d total Mbytes of physical memory.\n"), WIDTH, statex.ullTotalPhys / DIV);
+//	_tprintf(TEXT("There are %*I64d free Mbytes of physical memory.\n"), WIDTH, statex.ullAvailPhys / DIV);
+//	_tprintf(TEXT("There are %*I64d total Mbytes of paging file.\n"), WIDTH, statex.ullTotalPageFile / DIV);
+//	_tprintf(TEXT("There are %*I64d free Mbytes of paging file.\n"), WIDTH, statex.ullAvailPageFile / DIV);
+//	_tprintf(TEXT("There are %*I64d total Mbytes of virtual memory.\n"), WIDTH, statex.ullTotalVirtual / DIV);
+//	_tprintf(TEXT("There are %*I64d free Mbytes of virtual memory.\n"), WIDTH, statex.ullAvailVirtual / DIV);
+//	_tprintf(TEXT("There are %*I64d free Mbytes of extended memory.\n"), WIDTH, statex.ullAvailExtendedVirtual / DIV);
+//
+//
+//}
+//
+//
